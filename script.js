42c8ab89269bbb6a970946e79442e579b4f094cd
chanderslv <chanderslv@gmail.com>
2021-01-04 22:38:13 +0200

    update style and add JS script

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector(".dropdown").addEventListener("click", function () {
+    if (this.classList.contains("active")) {
+        document.querySelector(".side-menu").classList.remove("active");
+        this.classList.remove("active");
+      } else {
+        document.querySelector(".side-menu").classList.add("active");
+        this.classList.add("active");
+      }
+});
